9231fa5ecb9da0d2085e0917631667f609f02c7a
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-22 20:00:24 -0300

    Update personagem.js

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -19,15 +19,22 @@ class Personagem{
       [440, 810],
       [660, 810],
     ];
-    this.index = 0;
+    this.frameAtual=4;
   
   }
   exibe(){
-    image(this.imagem,55,height-320,110,135,this.matriz[this.index][0],this.matriz[this.index][1],220,270);
-    this.index++;
-    if(this.index >= this.matriz.length - 1) {
-      this.index = 0;
+    image(this.imagem,55,height-320,110,135,this.matriz[this.frameAtual][0],this.matriz[this.frameAtual][1],220,270);
+    this.anima();
     
   }
+  anima(){
+    this.frameAtual++;
+    if(this.frameAtual>=this.matriz.length-1){
+      this.frameAtual=0;
+      
+       
+       }
+       
   
+  }
 }
