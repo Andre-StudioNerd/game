53f299da872d98ad2610a2a5d54f6dd5b6dce2ca
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-22 15:32:24 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario=new Cenario(imagemCenario,3);
   personagem=new Personagem(imagemPersonagem);
-  frameRate(42);
+  frameRate(20);
   somDoJogo.loop();
   
 }
