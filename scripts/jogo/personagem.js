69a82f3ca627c8bd835c8dacdfcd09721d6d13a4
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-22 20:14:50 -0300

    Update personagem.js

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,39 +1,60 @@
 class Personagem{
   constructor(imagem){
-  this.imagem=imagem;
-    this.matriz = [
-      [0, 0],
-      [220, 0],
-      [440, 0],
-      [660, 0],
-      [0, 270],
-      [220, 270],
-      [440, 270],
-      [660, 270],
-      [0, 540],
-      [220, 540],
-      [440, 540],
-      [660, 540],
-      [0, 810],
-      [220, 810],
-      [440, 810],
-      [660, 810],
-    ];
-    this.frameAtual=4;
+    this.altura = 135;
+    this.largura = 110;
+    
+    // Caracteristicas do sprite utilizado
+    this.imagem = imagem;      // Sprite utilizado
+    this.spriteAltura = 270;   // Altura do personagem dentro do sprite
+    this.spriteLargura = 220;  // Largura do personagem dentro do sprite
+    // Variaveis auxiliares para o sprite
+    this.spriteI = 0;          // Index de coluna 
+    this.spriteJ = 0;          // Index de linha
+    //Posições dentro do sprite
+    this.posicapSpriteX = 0;
+    this.posicaoSpriteY = 0;
+    
+    // Posicionamento na tela
+    this.posicaoX = 0;
+    this.posicaoY = height - this.altura;
+  
   
   }
   exibe(){
-    image(this.imagem,55,height-320,110,135,this.matriz[this.frameAtual][0],this.matriz[this.frameAtual][1],220,270);
-    this.anima();
+    image(this.imagem,
+          
+      this.posicaoX, 
+      this.posicaoY, 
+      this.largura, 
+      this.altura,
+      
+      this.posicaoSpriteX, 
+      this.posicaoSpriteY, 
+      this.spriteLargura, 
+      this.spriteAltura
+     );
+
+this.anima()
     
   }
   anima(){
-    this.frameAtual++;
-    if(this.frameAtual>=this.matriz.length-1){
-      this.frameAtual=0;
+    this.spriteI++ ;
+    
+    if (this.spriteI >3) {
+      this.spriteI = 0;
+      this.spriteJ ++ ;
+      
+      if (this.spriteJ >3){
+        this.spriteJ = 0;  
+        }
+      }
+  
+    // Atualiza a posição inicial dentro do sprite
+    this.posicaoSpriteX = this.spriteI * this.spriteLargura;
+    this.posicaoSpriteY = this.spriteJ * this.spriteAltura;
       
        
-       }
+       
        
   
   }
