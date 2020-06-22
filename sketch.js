1b27a06ac33805a6b6c77cfbf373ae903f242cb4
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-22 20:59:29 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ let personagem;
 
 function preload(){
 imagemCenario=loadImage('imagens/cenario/fundo_game.png');
-imagemPersonagem=loadImage('imagens/personagem/megaman.png');
+imagemPersonagem=loadImage('imagens/personagem/megamanx.png');
 somDoJogo=loadSound('sons/abel.mp3');
 }
 
