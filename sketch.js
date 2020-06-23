f0967cad12e1bd5c12763c5a5138b834ec6e90ca
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-23 10:05:57 -0300

    up

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ let personagem;
 
 function preload(){
 imagemCenario=loadImage('imagens/cenario/fundo_game.png');
-imagemPersonagem=loadImage('imagens/personagem/shura.png');
+imagemPersonagem=loadImage('imagens/personagem/shura_corre.png');
 somDoJogo=loadSound('sons/abel.mp3');
 }
 
