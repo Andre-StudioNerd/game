5857988ed116b9c628d62957f27951a5dde601c4
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-23 15:58:15 -0300

    Update personagem.js

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,6 +1,6 @@
-class Personagem{
+class Personagem {
   constructor(imagem){
-  this.imagem=imagem;
+    this.imagem = imagem;
     this.matriz = [
       [0, 0],
       [220, 0],
@@ -13,28 +13,26 @@ class Personagem{
       [0, 540],
       [220, 540],
       [440, 540],
-      [660, 540],
+      [660, 540], 
       [0, 810],
       [220, 810],
       [440, 810],
       [660, 810],
-    ];
-    this.frameAtual=4;
-  
+    ]
+    this.frameAtual = 0
   }
+  
   exibe(){
-    image(this.imagem,55,height-320,110,135,this.matriz[this.frameAtual][0],this.matriz[this.frameAtual][1],220,270);
-    this.anima();
+    image(this.imagem, 0, height - 135, 110, 135, this.matriz[this.frameAtual][0], this.matriz[this.frameAtual][1], 220, 270);
     
+    this.anima()
   }
-  anima(){
-    this.frameAtual++;
-    if(this.frameAtual>=this.matriz.length-1){
-      this.frameAtual=0;
-      
-       
-       }
-       
   
+  anima(){
+    this.frameAtual++
+    
+    if(this.frameAtual >= this.matriz.length - 1) {
+      this.frameAtual = 0
+    }
   }
 }
