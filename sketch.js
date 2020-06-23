bb134542895026594980f8392f372d1c675bf071
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-23 13:18:41 -0300

    up

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ let personagem;
 
 function preload(){
 imagemCenario=loadImage('imagens/cenario/fundo_game.png');
-imagemPersonagem=loadImage('imagens/personagem/seiya.png');
+imagemPersonagem=loadImage('imagens/personagem/seiya_vai.png');
 somDoJogo=loadSound('sons/intro.mp3');
 }
 
