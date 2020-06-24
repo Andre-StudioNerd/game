d6ddeb4607fcefc0fb9b4f3835ddc07874083d83
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-24 12:59:33 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -83,7 +83,7 @@ const matrizSeiya = [
 
 function preload(){
 imagemCenario=loadImage('imagens/cenario/fundo_game.png');
-imagemPersonagem=loadImage('imagens/personagem/sprite_seiya.png');
+imagemPersonagem=loadImage('imagens/personagem/seiya_vai.png');
 imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
 imagemGameOver = loadImage('imagens/sistema/gameover.jpg');
 
@@ -95,7 +95,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario=new Cenario(imagemCenario,10);
   inimigo = new Inimigo(imagemInimigo, matrizInimigo, width-55, height-240, 50, 50, 104, 104);
-  personagem=new Personagem(imagemPersonagem,matrizSeiya,55, height-240, 73, 137, 73, 137);
+  personagem=new Personagem(imagemPersonagem,matrizpersonagem,55, height-320, 110, 135, 220, 270);
   
   frameRate(5);
   somDoJogo.loop();
