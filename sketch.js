3f5293e7e3023f8391a09d8af8cd035bbadc12dc
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-24 13:42:03 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -66,7 +66,7 @@ const matrizPersonagem = [
 
 function preload(){
 imagemCenario=loadImage('imagens/cenario/fundo_game.png');
-imagemPersonagem=loadImage('imagens/personagem/cdz.png');
+imagemPersonagem=loadImage('imagens/personagem/vai_seiya.png');
 imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
 imagemGameOver = loadImage('imagens/sistema/gameover.jpg');
 
