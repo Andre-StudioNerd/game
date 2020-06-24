3627f6cb2930461f6fa0f7cd6b17decf172da7cb
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-24 14:11:42 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -108,8 +108,8 @@ somPulo = loadSound('sons/jump.mp3');
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario=new Cenario(imagemCenario,10);
-  inimigo = new Inimigo(imagemInimigo, matrizInimigo, width-55, height-240, 50, 50, 104, 104);
-  personagem=new Personagem(imagemPersonagem,matrizInimigo,55, height-240, 50, 50, 104, 104);
+  inimigo = new Inimigo(imagemInimigo, matrizInimigo, width-55, height-240, 50, 50, 96, 96);
+  personagem=new Personagem(imagemPersonagem,matrizInimigo,55, height-240, 50, 50, 96, 96);
   
   frameRate(5);
   somDoJogo.loop();
