c3fd082113dc92b1eefa192812d1e1532a5f28cc
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-25 12:04:21 -0300

    up

--- sketch.js
+++ sketch.js
@@ -102,7 +102,7 @@ const matrizPersonagem = [
 function preload(){
 imagemCenario=loadImage('imagens/cenario/fundo_game.png');
 imagemPersonagem=loadImage('imagens/personagem/pegaso.png');
-imagemInimigo = loadImage('imagens/inimigos/docrates.png');
+imagemInimigo = loadImage('imagens/inimigos/docrates_negro.png');
 imagemParalaxe=loadImage('imagens/cenario/santuario.png');
 
 
