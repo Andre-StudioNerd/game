bb5994c10735ea5940918d769929968bfa4158fc
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-25 12:49:06 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -101,7 +101,7 @@ const matrizPersonagem = [
 
 function preload(){
 imagemCenario=loadImage('imagens/cenario/fundo_game.png');
-imagemPersonagem=loadImage('imagens/personagem/pegaso.png');
+imagemPersonagem=loadImage('imagens/personagem/seiya_pegasus.png');
 imagemInimigo = loadImage('imagens/inimigos/docrates.png');
 imagemParalaxe=loadImage('imagens/cenario/santuario.png');
 
