934cc1c0c606ccce8a1cd9394dd15c5216a10f99
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-25 11:34:08 -0300

    Update personagem.js

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,12 +1,12 @@
 class Personagem extends Animacao {
   constructor(imagem, matriz, x, y, largura, altura, lSprite, aSprite, somPulo) {
     super(imagem, matriz, x, y, largura, altura, lSprite, aSprite);
-    //this.somPulo = somPulo;
+    this.somPulo = somPulo;
     this.y0 = y;
     
     this.vPulo = 0;
     this.gravidade = 20;
-    this.qntPulos = 2;
+    this.qntPulos = 5;
    
 
   }
@@ -15,7 +15,7 @@ class Personagem extends Animacao {
     if (this.qntPulos > 0) {
       this.vPulo = -50;
       this.qntPulos--;
-      //this.somPulo.play();
+      this.somPulo.play();
     }
   }
   simularGravidade() {
@@ -24,7 +24,7 @@ class Personagem extends Animacao {
     
     if (this.y > this.y0){
       this.y = this.y0;
-      this.qntPulos = 2;
+      this.qntPulos = 5;
     }
   }
 
